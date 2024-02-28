3e1da678d6e5c7a7d7d7af0fce07ff2d185f85a2
Yamijke <serhei.knyazev@gmail.com>
2024-02-28 15:38:05 +0300

    5. Bean definitions [#505075]

---
+++ src/main/java/ru/job4j/di/xmlbased/Cat.java
@@ -0,0 +1,17 @@
+package ru.job4j.di.xmlbased;
+
+public class Cat {
+    private String name;
+    private Cat() {
+
+    }
+    public static Cat createCat(String name) {
+        Cat cat = new Cat();
+        cat.name = name;
+        return cat;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
