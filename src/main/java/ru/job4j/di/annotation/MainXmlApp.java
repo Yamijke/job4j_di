84e79019dd8e4b2e477613f84b549d246c191833
Yamijke <serhei.knyazev@gmail.com>
2024-02-28 15:05:50 +0300

    4. Конфигурация контейнера [#505072 #483761]

---
+++ src/main/java/ru/job4j/di/annotation/MainXmlApp.java
@@ -0,0 +1,13 @@
+package ru.job4j.di.annotation;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+
+public class MainXmlApp {
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+        Man man = context.getBean(Man.class);
+        man.print();
+    }
+}
