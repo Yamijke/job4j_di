2b7b2f66054807e1c44f8e9a60e43e9f44c4d56b
Yamijke <serhei.knyazev@gmail.com>
2024-05-16 12:55:22 +0300

    0. Что такое DI? [#6876]

--- src/main/java/ru/job4j/di/NoContextDI.java
+++ src/main/java/ru/job4j/di/NoContextDI.java
@@ -3,7 +3,8 @@ package ru.job4j.di;
 public class NoContextDI {
     public static void main(String[] args) {
         Store store = new Store();
-        StartUI ui = new StartUI(store);
+        ConsoleInput consoleInput = new ConsoleInput();
+        StartUI ui = new StartUI(store, consoleInput);
         ui.add("Petr Arsentev");
         ui.add("Ivan ivanov");
         ui.print();
--- src/main/java/ru/job4j/di/StartUI.java
+++ src/main/java/ru/job4j/di/StartUI.java
@@ -5,9 +5,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class StartUI {
     private Store store;
+    private ConsoleInput consoleInput;
 
-    public StartUI(Store store) {
+    public StartUI(Store store, ConsoleInput consoleInput) {
         this.store = store;
+        this.consoleInput = consoleInput;
     }
 
     public void add(String value) {
